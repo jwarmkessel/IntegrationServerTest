58ef1f18db9dbf1d392bb70f61b927e70231ce5e
Justin Warmkessel <justin.w@ymedialabs.com>
2015-10-19 13:59:53 -0700

    Committing tewt.

--- INTEGRATIONSERVERTESTTests/INTEGRATIONSERVERTESTTests.swift
+++ INTEGRATIONSERVERTESTTests/INTEGRATIONSERVERTESTTests.swift
@@ -24,6 +24,8 @@ class INTEGRATIONSERVERTESTTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCTAssertTrue(1 == 1)
     }
     
     func testPerformanceExample() {
